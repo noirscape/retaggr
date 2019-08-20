130f7c6cd128327a9d00ce28cfec1808ff623502
noirscape <neko@catgirlsin.space>
2019-08-20 12:21:16 +0200

    Test individual search and all search

--- tests/test_core.py
+++ tests/test_core.py
@@ -27,3 +27,15 @@ async def test_core_search_image_not_all_api_keys():
     core = retaggr.ReverseSearch(retaggr.ReverseSearchConfig()) # Since we need a core without the config for this
     with pytest.raises(retaggr.MissingAPIKeysException):
         await core.search_image("danbooru", "irrelevant")
+
+@pytest.mark.asyncio
+async def test_image_core():
+    core = retaggr.ReverseSearch(config)
+    tags = await core.search_image("paheal", "https://iris.paheal.net/_images/f0a277f7c4e80330b843f8002daf627e/1876780%20-%20Dancer_of_the_Boreal_Valley%20Dark_Souls%20Dark_Souls_3%20Sinensian.jpg")
+    assert tags == ['Dancer_of_the_Boreal_Valley', 'Dark_Souls', 'Dark_Souls_3', 'Sinensian']
+
+@pytest.mark.asyncio
+async def test_reverse_search():
+    core = retaggr.ReverseSearch(config)
+    tags = await core.search_image("paheal", "https://iris.paheal.net/_images/f0a277f7c4e80330b843f8002daf627e/1876780%20-%20Dancer_of_the_Boreal_Valley%20Dark_Souls%20Dark_Souls_3%20Sinensian.jpg")
+    assert tags == ['Dancer_of_the_Boreal_Valley', 'Dark_Souls', 'Dark_Souls_3', 'Sinensian']
