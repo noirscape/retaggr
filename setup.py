7d64e7186b4ecfa86c480766d9de7321a9312cf4
noirscape <neko@catgirlsin.space>
2019-08-20 23:00:13 +0200

    1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="retaggr",
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/booru-utils/reverse-search",
     license="LGPLv3",
     description="Reverse image searching utility for images.",
