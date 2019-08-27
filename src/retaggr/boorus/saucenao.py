34749d29583628982d05e8184976b3666e72e237
noirscape <neko@catgirlsin.space>
2019-08-27 15:15:13 +0200

    Start some work on figuring out what indexes we have a use for

--- src/retaggr/boorus/saucenao.py
+++ src/retaggr/boorus/saucenao.py
@@ -1,6 +1,6 @@
 from retaggr.boorus.base import Booru
 from retaggr.errors import NotAvailableSearchOption
-from aiohttp_requests import requests
+import requests as fuck_aiohttp
 
 class SauceNao(Booru):
     """Reverse searches the SauceNao API and then does additional matching.
@@ -8,11 +8,26 @@ class SauceNao(Booru):
     This booru does not require images to be downloaded before searching.
 
     This API is subject to rate limits.
+
     :param api_key: SauceNao API key. You can get this by registering an account on saucenao.com
     :type api_key: str
     """
     host = "https://saucenao.com"
     download_required = False
+
+
+    compatible_indexes = set([9, 12, 26, 29])
+    """All compatible indexes we can parse.
+
+    Valid index numbers can be found at https://saucenao.com/status.html .
+
+    List of indexes we can parse:
+
+    * 9: Danbooru
+    * 12: Yande.re
+    * 26: Konachan
+    * 29: E621
+    """
 
     def __init__(self, api_key):
         self.api_key = api_key
@@ -22,10 +37,19 @@ class SauceNao(Booru):
         params = {
             "db": "999", # No clever bitmasking -> need help with how to do that.
             "api_key": self.api_key,
-            "output_type": "2" # 2 is the JSON API
+            "output_type": "2", # 2 is the JSON API,
+            "url": url
         }
-        r = await requests.get(request_url, params=params)
+        r = fuck_aiohttp.get(request_url, params=params)
         return r.json()
 
     async def search_tag(self, tag):
         raise NotAvailableSearchOption("This engine cannot search tags.")
+
+    async def index_parser(self, json):
+        """Parse the output from a succesful saucenao search to retrieve data from specific indexes.
+        
+        :param json: JSON output from the API.
+        :type json: dict
+        """
+        pass
